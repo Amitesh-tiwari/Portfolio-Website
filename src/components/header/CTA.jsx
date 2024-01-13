e1c509ec9827912bcc76a7fc075e2a647f98f9aa
Amitesh tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-01-13 12:42:14 +0530

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href  = {"C:\Users\amite\OneDrive\Pictures\engagement\New folder\Amitesh resume (1).pdf"}className='btn'> DowNload resume</a>
+        <a href  = " resume" className='btn'> DowNload resume</a>
         <a href = "#contact" className='btn btn-primary'> Lets talk</a>
       
     </div>
