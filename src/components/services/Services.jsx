d8537bb864daaf7f8565f8850c119599b3c2714c
Amitesh tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-01-13 12:47:10 +0530

    Update Services.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -10,51 +10,59 @@ const services = () => {
       <div className='services_container'>
         <article className='service'>
           <div className='service_head'>
-            <h3>UI/UX Design</h3>
+            <h3>The Basics </h3>
           </div>
         <ul className='service_list'>
           <li>
             <BsCheckLg className='service_list_icons'/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p> Database management.</p>
           </li>
 
           <li>
             <BsCheckLg className='service_list_icons'/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Operating Systems .</p>
           </li>
           <li>
             <BsCheckLg className='service_list_icons'/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Data Structures and Algorithms.</p>
           </li>
           <li>
             <BsCheckLg className='service_list_icons'/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Cloud Computing.</p>
           </li>
           <li>
             <BsCheckLg className='service_list_icons'/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Computer Networks.</p>
           </li>
           <li>
             <BsCheckLg className='service_list_icons'/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Communication and Leadership Skills.</p>
           </li>
-
+          
         </ul>
         </article>
         {/* end of UI/UX*/}
         <article className='service'>
           <div className='service_head'>
-            <h3>Web Development</h3>
+            <h3>Development</h3>
           </div>
         <ul className='service_list'>
           <li>
             <BsCheckLg className='service_list_icons'/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>HTML</p>
           </li>
 
           <li>
             <BsCheckLg className='service_list_icons'/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>React JS.</p>
+          </li>
+          <li>
+            <BsCheckLg className='service_list_icons'/>
+            <p>CSS</p>
+          </li>
+          <li>
+            <BsCheckLg className='service_list_icons'/>
+            <p>JavaScript</p>
           </li>
           <li>
             <BsCheckLg className='service_list_icons'/>
@@ -62,50 +70,43 @@ const services = () => {
           </li>
           <li>
             <BsCheckLg className='service_list_icons'/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Devops and CI/CD pipleine.</p>
           </li>
-          <li>
-            <BsCheckLg className='service_list_icons'/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          </li>
-          <li>
-            <BsCheckLg className='service_list_icons'/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-          </li>
-
+          
         </ul>
         </article>
 
         <article className='service'>
           <div className='service_head'>
-            <h3>Content creation</h3>
+            <h3>Coding</h3>
           </div>
         <ul className='service_list'>
           <li>
             <BsCheckLg className='service_list_icons'/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Solved 100+ questions combined.</p>
           </li>
 
           <li>
             <BsCheckLg className='service_list_icons'/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Acheiver at CodeStudio.</p>
           </li>
           <li>
             <BsCheckLg className='service_list_icons'/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Skilled in Problem Solving.</p>
           </li>
           <li>
             <BsCheckLg className='service_list_icons'/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>4⭐@HackerRank in C and C++.</p>
           </li>
           <li>
             <BsCheckLg className='service_list_icons'/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Solved 50+ questions combined on different paltforms.</p>
           </li>
           <li>
             <BsCheckLg className='service_list_icons'/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <p>Skilled in Puzzle solving.</p>
           </li>
+
         </ul>
         </article>
       </div>
@@ -114,3 +115,4 @@ const services = () => {
 }
 
 export default services
+
