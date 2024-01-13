1488023f4fcf69b8eb6987e24ef75a393e639fd7
Amitesh tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-01-13 12:48:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Nav from './components/nav/Nav'
 import Experience from './components/experience/Experience'
 import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
-import Testimonial from './components/testimonials/Testimonial'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
@@ -19,7 +18,6 @@ const App = () => {
     <Experience/>
     <Services/>
     <Portfolio/>
-    <Testimonial/>
     <Contact/>
     <Footer/>
     </>
