62ce848ad42f5419c3204dbc294de0b33bfe42a7
Amitesh tiwari <113535850+Amitesh-tiwari@users.noreply.github.com>
2024-01-13 12:44:19 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,9 +7,9 @@ import {BsGithub} from 'react-icons/bs'
 const footer = () => {
   return (
     <footer>
-      <a href='#' className='footer__logo'>Amitesh Tiwari</a>
+      <a href='#logo' className='footer__logo'>Amitesh Tiwari</a>
       <ul className='permalinks'>
-        <li><a href='#'>Home</a></li>
+        <li><a href='#home'>Home</a></li>
         <li><a href='#about'>About</a></li>
         <li><a href='#experience'>Experience</a></li>
         <li><a href='#services'>Services</a></li>
@@ -31,3 +31,4 @@ const footer = () => {
 }
 
 export default footer
+
